28e1065dc275b83d8afb6b8331a8a9c5a3197d2f
Aleksandr Shishkin <elixor@mail.ru>
2023-02-23 13:07:54 +0300

    Added createComment + PostNotFoundException + two tests

--- src/main/kotlin/ru.netology/main.kt
+++ src/main/kotlin/ru.netology/main.kt
@@ -1,6 +1,9 @@
 package ru.netology
 
 import ru.netology.attachments.*
+import java.lang.RuntimeException
+
+//public inline fun TODO(reason: String): Nothing = throw NotImplementedError("An operation is not implementer: $reason")
 
 
 data class Post(
@@ -17,9 +20,28 @@ data class Post(
     var attachments: Array<Attachments>     //массив вложений
 )
 
+class Comment(
+    var id: Int,                            //Идентификатор комментария
+    val fromId: Int,                        //Идентификатор автора комментария
+    val date: Long,                         //Дата
+    val text: String,                       //Текст комментария
+    var attachments: Array<Attachments>     //массив вложений
+)
+
 object WallService {
     private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
     private var nextId: Int = 0
+    private var nextCommentId: Int = 0
+
+    fun createComment(postId: Int, comment: Comment): Comment {     //метод добавления комментария
+        if (searchById(postId) != null) {
+            comments += comment
+            comment.id = ++nextCommentId
+            return comments.last()
+        }
+        throw PostNotFoundException("No post with id $postId")
+    }
 
     fun add(post: Post): Post {                                     //метод добавления поста
         posts += post
@@ -52,53 +74,49 @@ object WallService {
         return false
     }
 
-    fun searchById(id: Int): Post {                                       //метод поиска поста по его id
-        for ((index, post) in posts.withIndex()) {
+    fun searchById(id: Int): Post? {                                       //метод поиска поста по его id
+        for (post in posts) {
             if (post.id == id) {
                 return post
             }
         }
-        return TODO()
+        return null
     }
 
 
 
 }
 
+class PostNotFoundException (message: String) : RuntimeException(message)
+
 fun main() {
 
-    val emptyStringArray = arrayOf<Attachments>()   //задаем пустой массив вложений поста
+    //val emptyStringArray = arrayOf<Attachments>()   //задаем пустой массив вложений поста
     //публикуем посты
-    val post1 = Post(0,9,6, true,0,"ПОСТ #1 на стене пользователя 9 автора 6",17,3,true,false, emptyStringArray)
+    val post1 = Post(0,9,6, true,0,"ПОСТ #1 на стене пользователя 9 автора 6",17,3,true,false, arrayOf<Attachments>())
     WallService.add(post1)
     println(post1)
-    val post2 = Post(0,6,4, false,0,"ПОСТ #2 на стене пользователя 6 автора 4",25,10,false,false, emptyStringArray)
+    val post2 = Post(0,6,4, false,0,"ПОСТ #2 на стене пользователя 6 автора 4",25,10,false,false, arrayOf<Attachments>())
     WallService.add(post2)
     println(post2)
-    val post3 = Post(0,4,9, true,0,"ПОСТ #3 на стене пользователя 4 автора 9",68,18,false,false, emptyStringArray)
+    val post3 = Post(0,4,9, true,0,"ПОСТ #3 на стене пользователя 4 автора 9",68,18,false,false, arrayOf<Attachments>())
     WallService.add(post3)
     println(post3)
 
-    //отображаем пост #2
-    println(WallService.searchById(2))
-
     //обновляем пост #2 (для наглядности в поле text добавим слово ОБНОВЛЕННЫЙ)
-    val (id, ownerId, fromId, friendsOnly, date, text, views, likes, isPinned, isFavourite, attachments) = WallService.searchById(2)
-    val post2Edited = Post(id, ownerId, fromId, friendsOnly, date, "ОБНОВЛЕННЫЙ " + text,views, likes, isPinned, isFavourite, attachments)
+    val post2Edited = Post(2,6,4, false,0,"ОБНОВЛЕННЫЙ " + "ПОСТ #2 на стене пользователя 6 автора 4",25,10,false,false, arrayOf<Attachments>())
     WallService.update(post2Edited)
-
-    //отображаем уже обновленный пост #2
-    println(WallService.searchById(2))
-
     //также лайкнем пост #2
     WallService.likeById(2)
-
-    //теперь отобразим лайкнутый пост #2
+    //теперь отобразим обновленный и лайкнутый пост #2
     println(WallService.searchById(2))
 
+    val commentForPost = Comment(1,59,0,"комментарий", arrayOf<Attachments>())
 
-    //val liked = post.copy(likes = post.likes + 1)
-    //val (id, ownerId, _, _, _, text) = post
-    //println(liked)
-    //println("$id, $ownerId, $text, $ownerId")
+    // добавление комментария к существующему посту #2
+    val id = 10
+    val textComment = WallService.createComment(id, commentForPost).text
+    val textPost = WallService.searchById(id)?.text
+    println("К ПОСТУ: " + textPost + " ДОБАВЛЕН КОММЕНТАРИЙ: " + textComment)
+
 }
--- src/test/kotlin/ru/netology/Tests.kt
+++ src/test/kotlin/ru/netology/Tests.kt
@@ -67,3 +67,47 @@ class UpdateTest {
         assertEquals(false, result)
     }
 }
+
+class CreateCommentTest {
+
+    @Test
+    fun createComment() {
+        //вначале создаем пост, мы же не можем проверить добавление комментария к несуществующему посту, т.к. всегда будет ошибка из-за отсутствующего поста
+        val id = 1
+        val ownerId = 9
+        val fromId = 6
+        val friendsOnly = true
+        val date: Long = 0
+        val text = "ПОСТ #1 на стене пользователя 9 автора 6"
+        val views = 17
+        val likes = 3
+        val isPinned = true
+        val isFavourite = false
+        val emptyStringArray = arrayOf<Attachments>()   //задаем пустой массив вложений поста
+        WallService.add(Post(id, ownerId, fromId, friendsOnly, date, text, views, likes, isPinned, isFavourite, emptyStringArray)).id
+
+        //теперь создаем комментарий
+        val postId = 1                                       //К какому посту добавляем комментарий
+        var commentId = 1                                    //Идентификатор комментария
+        val commentFromId = 1                                //Идентификатор автора комментария
+        val commentDate: Long = 0                            //Дата
+        val commentText = "текст комментария"                //Текст комментария
+        var commentEmptyStringArray = arrayOf<Attachments>() //массив вложений
+        val result = WallService.createComment(postId, Comment(commentId, commentFromId, commentDate, commentText, commentEmptyStringArray)).id
+
+        //проверяем на соответствие ожидаемому результату
+        assertEquals(1, result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        //создаем только комментарий, сам пост не нужен, т.к. мы будем пытаться добавить комментарий к несуществующему посту чтобы выкинуло ошибку
+        val postId = 1                                       //К какому посту добавляем комментарий
+        var commentId = 1                                    //Идентификатор комментария
+        val commentFromId = 1                                //Идентификатор автора комментария
+        val commentDate: Long = 0                            //Дата
+        val commentText = "текст комментария"                //Текст комментария
+        var commentEmptyStringArray = arrayOf<Attachments>() //массив вложений
+        WallService.createComment(postId, Comment(commentId, commentFromId, commentDate, commentText, commentEmptyStringArray))
+    }
+}
